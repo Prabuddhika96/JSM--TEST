c17fe91fa9eb630950184e979a5bac03bd769fc0
prabuddhika.m <prabuddhika.m@eyepax.com>
2024-06-27 18:37:35 +0530

    get task data

---
+++ create-task-util.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const createTask = async (issueData) => {
+  const username = process.env.JIRA_USERNAME;
+  const apiToken = process.env.JIRA_API_TOKEN;
+  const domain = process.env.JIRA_DOMAIN;
+
+  // Base64 encode the credentials for Basic Authentication
+  const token = Buffer.from(`${username}:${apiToken}`, "utf8").toString(
+    "base64"
+  );
+  const authHeader = `Basic ${token}`;
+
+  const baseUrl = `https://${domain}.atlassian.net`;
+  const endpoint = `${baseUrl}/rest/api/3/issue`;
+
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: authHeader,
+    },
+  };
+
+  try {
+    const response = await axios.post(endpoint, issueData, config);
+    console.log("Issue created:", response.data);
+    return res.status(200).send(response.data);
+  } catch (error) {
+    console.error("Error creating issue:", error.message);
+    return res.status(500).send({ error: error.message });
+  }
+};
--- server.js
+++ server.js
@@ -17,43 +17,57 @@ app.post("/webhook", async (req, res) => {
     const { issue } = req.body;
     const issueKey = issue.key;
     const summary = issue.fields.summary;
-    console.log(`Issue Key: ${issueKey}`);
-    console.log(`Summary: ${summary}`);
 
-    // Example: Call Jira API to create an issue
-    const jiraBaseUrl = "https://prabuddhika1996.atlassian.net";
-    const createIssueUrl = `${jiraBaseUrl}/rest/api/3/issue`;
-
-    const jiraAuth = {
-      username: "prabuddhika1996@gmail.com",
-      password: process.env.API_KEY, // Or use API token here
-    };
-
-    const issueData = {
-      fields: {
-        project: {
-          key: "TT",
-        },
-        summary: "task from node js app",
-        issuetype: {
-          name: "Task",
-        },
-      },
-    };
-
-    const response = await axios.post(createIssueUrl, issueData, {
-      auth: jiraAuth,
-    });
-
-    console.log("Issue created in Jira:", response.data);
-    res
-      .status(200)
-      .send("Webhook received and issue created successfully in Jira");
+    console.log(`issue: ${issue}`);
+    // console.log(`Issue Key: ${issueKey}`);
+    // console.log(`Summary: ${summary}`);
   } catch (error) {
-    console.error("Error creating issue in Jira:", error.message);
-    res.status(500).send("Error processing webhook");
+    console.error("Error:", error.message);
   }
 });
+
+// app.post("/issue-create", async function (req, res) {
+//   const username = process.env.JIRA_USERNAME;
+//   const apiToken = process.env.JIRA_API_TOKEN;
+//   const domain = process.env.JIRA_DOMAIN;
+
+//   // Base64 encode the credentials for Basic Authentication
+//   const token = Buffer.from(`${username}:${apiToken}`, "utf8").toString(
+//     "base64"
+//   );
+//   const authHeader = `Basic ${token}`;
+
+//   const baseUrl = `https://${domain}.atlassian.net`;
+//   const endpoint = `${baseUrl}/rest/api/3/issue`;
+
+//   const data = {
+//     fields: {
+//       project: {
+//         key: "JS",
+//       },
+//       summary: "Task from Node.js",
+//       issuetype: {
+//         name: "Task",
+//       },
+//     },
+//   };
+
+//   const config = {
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: authHeader,
+//     },
+//   };
+
+//   try {
+//     const response = await axios.post(endpoint, data, config);
+//     console.log("Issue created:", response.data);
+//     return res.status(200).send(response.data);
+//   } catch (error) {
+//     console.error("Error creating issue:", error.message);
+//     return res.status(500).send({ error: error.message });
+//   }
+// });
 
 const PORT = 3000;
 app.listen(PORT, () => {
